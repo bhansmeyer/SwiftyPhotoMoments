cb9423b998ed9492e4686d9f88b4e0b37a2010bf
Becky Hansmeyer <becky.brewer@doane.edu>
2018-09-19 14:17:04 -0500

    Initial Commit

---
+++ SwiftyPhotoMoments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyPhotoMoments
+//
+//  Created by Becky Hansmeyer on 9/19/18.
+//  Copyright © 2018 Becky Hansmeyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
